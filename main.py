25d7296326b81fb20ccc9ff36976bb628fa79588
jaanonim <mat8mro@gmail.com>
2021-11-23 19:37:48 +0100

    py game template

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
+import pygame
+
 
 def main():
-	pass
+	pygame.init()
+
+	screen = pygame.display.set_mode([500, 500])
+
+	running = True
+	while running:
+
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				running = False
+
+		screen.fill((255, 255, 255))
+
+		pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+		pygame.display.flip()
+	pygame.quit()
 
 
 if __name__ == "__main__":
