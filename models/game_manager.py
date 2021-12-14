c2df5fad1a3d277f125216e8c8f9ff9b81bbbfce
n2oneProgrammer <dominikwojtasik191@gmail.com>
2021-12-14 20:17:50 +0100

    Implement library pymunk for physic

--- models/game_manager.py
+++ models/game_manager.py
@@ -8,6 +8,8 @@ class GameManager(metaclass=Singleton):
     def __init__(self, width=800, height=600):
         self.width = width
         self.height = height
+
+        self._clock = pygame.time.Clock()
         self.screen = None
 
     def run(self):
@@ -24,6 +26,13 @@ class GameManager(metaclass=Singleton):
                 if event.type == pygame.QUIT:
                     running = False
             self.screen.fill((255, 255, 255))
-            scene.update()
+            if self._clock.get_fps() == 0:
+                dt = 0
+            else:
+                dt = 1.0 / self._clock.get_fps()
+            scene.update(dt)
             pygame.display.flip()
+            self._clock.tick(60)
+
+            pygame.display.set_caption("fps: " + str(self._clock.get_fps()))
         pygame.quit()
--- models/graphics/scenes/scene.py
+++ models/graphics/scenes/scene.py
@@ -2,7 +2,7 @@ class Scene:
     def __init__(self):
         self.objects = []
 
-    def update(self):
+    def update(self, delta_time):
         for obj in self.objects:
             from models.game_manager import GameManager
             obj.draw(GameManager().screen)
