dc8c5a95bcf2bf9c01617ababa8d6ca36d390bfc
n2oneProgrammer <kubawojtasik70@gmail.com>
2022-01-27 17:12:52 +0100

    resizing window

--- models/game_manager.py
+++ models/game_manager.py
@@ -21,7 +21,8 @@ class GameManager(metaclass=Singleton):
     def run(self):
         pygame.init()
 
-        self.screen = pygame.display.set_mode([self.width, self.height])
+        self.screen = pygame.Surface([800, 600])
+        self.screen2 = pygame.display.set_mode([self.width, self.height], pygame.RESIZABLE)
 
         self.scene = Map()
 
@@ -30,6 +31,9 @@ class GameManager(metaclass=Singleton):
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
+                elif event.type == pygame.VIDEORESIZE:
+                    self.width = event.w
+                    self.height = event.h
                 elif self.player is not None:
                     self.player.run_events(event)
             if self._clock.get_fps() == 0:
@@ -37,6 +41,8 @@ class GameManager(metaclass=Singleton):
             else:
                 dt = 1.0 / self._clock.get_fps()
             self.scene.update(dt)
+            resize_screen = pygame.transform.scale(self.screen, (self.width, self.height))
+            self.screen2.blit(resize_screen, (0, 0))
             pygame.display.flip()
             self._clock.tick(60)
 
